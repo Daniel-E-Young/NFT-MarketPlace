6d54ae2bf9801d444eb2ca2866874ef0dab8c659
Daniel-E-Young <youngdanielebi@yahoo.com>
2022-09-14 11:59:33 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,100 @@
+// -----Hamburger-------//
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#fff';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+function reveal() {
+	var reveals = document.querySelectorAll(".reveal");
+  
+	for (var i = 0; i < reveals.length; i++) {
+	  var windowHeight = window.innerHeight;
+	  var elementTop = reveals[i].getBoundingClientRect().top;
+	  var elementVisible = 150;
+  
+	  if (elementTop < windowHeight - elementVisible) {
+		reveals[i].classList.add("active");
+	  } else {
+		reveals[i].classList.remove("active");
+	  }
+	}
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+
+
+// ----- End Hamburger-------//
+
+
+
+// ---------TImer-----------//
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 20, 2022 12:00:00").getTime();
+
+// Update the count down every 1 second
+let x;
+
+x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdwn").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdwn1").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdwn2").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdwn3").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdwn4").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdwn5").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdwn6").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdwn").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+// --------------END Timer-----------//
