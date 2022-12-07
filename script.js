e2439f05ad812cf402db619dd4a5c45f1a33212b
Daniel-E-Young <109361146+Daniel-E-Young@users.noreply.github.com>
2022-12-07 07:12:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function reveal() {
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 30, 2022 12:00:00").getTime();
+var countDownDate = new Date("Jan 15, 2023 12:00:00").getTime();
 
 // Update the count down every 1 second
 let x;
