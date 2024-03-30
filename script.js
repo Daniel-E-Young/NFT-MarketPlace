f2d318fe73731eb63bf2deda62ae5f1873a06a5b
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2024-03-30 10:52:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 2500) {
+	if (scroll_position > 250) {
 		header.style.backgroundColor = '#fff';
 	} else {
 		header.style.backgroundColor = 'transparent';
