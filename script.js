3c06c5b3161cf4fba258fc93265a813194f5fa44
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2024-04-16 14:39:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // -----Hamburger-------//
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li ');
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
